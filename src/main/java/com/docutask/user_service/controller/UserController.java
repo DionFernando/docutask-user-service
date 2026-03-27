8fe4161b2888a607846418c65998d1c0545465e8
DionFernando <dionfernando2003@gmail.com>
2026-03-27 09:56:43 +0530

    new commit with basics

---
+++ src/main/java/com/docutask/user_service/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.docutask.user_service.controller;
+
+import com.docutask.user_service.entity.User;
+import com.docutask.user_service.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@CrossOrigin
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
